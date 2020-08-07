5709e489207c66e5b01bbef0a8382b1e8991b621
Yue Su <su2047@gmail.com>
2020-08-07 12:42:35 -0300

    added useForm

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,18 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react'
+
+export const useForm = (initialValue) => {
+    const [values, setValue] = useState(initialValue);
+
+    const handleChanges = e => {
+        const { name, value } = e.target
+
+        setValue({
+            ...values,
+            [name]: value,
+        });
+    }
+
+    return [values, handleChanges];
+
+}
